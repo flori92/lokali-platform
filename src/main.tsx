8e74d958e79083bea69752fff9f5c5c0cfb58fee
FLo <florifavi@gmail.com>
2025-08-21 20:40:16 +0200

    🚀 DEPLOY: Build production pour GitHub Pages

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-console.log('[Lokali] Initializing React app...');
-createRoot(document.getElementById("root")!).render(<App />);
